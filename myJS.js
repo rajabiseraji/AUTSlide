e2a8c50384c7f587566d7efdd60264ff363b55d2
MrOlaf <rajabiseraji@outlook.com>
2016-08-07 17:37:11 +0430

    all shit break loose

--- myJS.js
+++ myJS.js
@@ -9,6 +9,11 @@
 
 $(document).ready(function(){
 
+    console.log("gooooz");
+    $("#kar").fadeIn(4000);
+    $("#dars").fadeIn(1000);
+    $("#code").fadeIn(7000);
+    $("#dars").addClass("fixed", 1000);
 
     setTimeout(function(){
         //console.log(window.location.href);
@@ -25,18 +30,76 @@ for(var i =0 ; i < arr.length ; i++){
     console.log("hello");
 }
 
-
+    $(".logo").animate({width: "90px" , height: "90px" , paddingTop : "1px" , opacity:"1"}, 3000, function(){
+        $("#logoCircle path").addClass("animated");
+    });
     $("#why").animate({"opacity" : "1"}, 3000)
-    .animate({fontSize : "48px" , paddingTop : "10px"}, 3000, function(){
-            $("#kar").fadeIn(4000);
-            $("#dars").fadeIn(1000);
-            $("#code").fadeIn(7000);
-            $("#dars").toggleClass("fixed", 4000);
+    .animate({fontSize : "48px" , paddingTop : "100px"}, 3000, function(){
+            //$(".logo").animate({width: "90px" , height: "90px" , paddingTop : "1px"}, 3000, function(){
+            //    $("#logoCircle path").addClass("animated");
+            //});
+
+            $("#sakhtLogo").fadeIn(1000);
+            $("#NarmLogo").fadeIn(2000);
+            $("#ITLogo").fadeIn(3000);
 
         })
 
 
 });
+
+
+
+/********************   **************************/
+
+$(document).keypress(function(){
+
+    if(window.location.href == "http://localhost:63342/SlideAUT/second.html") {
+
+
+        clickNum++;
+        console.log(clickNum);
+        console.log($("#dars").attr("class") + " kar "+ $("#kar").attr("class"));
+        if(clickNum == 1) {
+            $("#dars").removeClass("fixed", 2000);
+            //$("#dars").addClass("", 2000);
+            $("#kar").toggleClass("fixed", 2000);
+        } else if(clickNum == 2) {
+            //$("#dars").toggleClass("fixed", 2000);
+            $("#kar").toggleClass("fixed", 2000);
+            $("#code").toggleClass("fixed", 2000);
+        }
+
+
+    } else {
+
+        $("#why , #NarmLogo , #ITLogo, #compLogo , g").fadeOut(1000);
+        //$("g").hide(1000);
+        //$("").hide(1000);
+        //$("#ITLogo").hide(1000);
+        //$("#compLogo").hide(1000);
+        $("#sakhtLogo span").fadeOut(1000, function () {
+            document.location.href = "second.html"
+        });
+        $("#sakhtLogo").animate({
+            top: "5px",
+            right: "50px",
+            backgroundSize: "100%",
+            width: "150px",
+            height: "130px"
+        }, 2000, function () {
+        });
+
+    }
+
+
+});
+
+
+
+
+
+
 
 var clickNum = 0;
 
